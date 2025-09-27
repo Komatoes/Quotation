8eaefe6368ffef49bef47f78625b2ab020d3ec3a
Komatoes <laronvogn@gmail.com>
2025-09-27 17:02:12 +0800

    made Accept,reject, and drafts table for Quotation

---
+++ app/Models/QuotationStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuotationStatus extends Model
+{
+    use HasFactory;
+
+    // Tell Laravel the exact table name
+    protected $table = 'quotation_status';
+
+    protected $fillable = [
+        'status_name',
+    ];
+
+    // One status can belong to many quotations
+    public function quotations()
+    {
+        return $this->hasMany(Quotation::class, 'status_id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\QuotationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MaterialController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,38 +19,41 @@ use App\Http\Controllers\MaterialController;
 
 Route::get('/', [QuotationController::class, 'viewHome']);
 
-Route::get('/quotations/{id}', [QuotationController::class, 'show'])->name('quotations.show');
+// Put this BEFORE the {id} route
+Route::get('/quotations/drafts', [QuotationController::class, 'drafts']);
+
+// Restrict {id} to numbers so it won’t catch “drafts”
+Route::get('/quotations/{id}', [QuotationController::class, 'show'])
+    ->whereNumber('id')
+    ->name('quotations.show');
 
 // Route::get('/login', [AuthenticationController::class, 'viewLogin']);
-
 // Route::post('/login-user', [AuthenticationController::class, 'loginUser']);
-
 // Route::get('/logout-user', [AuthenticationController::class, 'logoutUser']);
-
 // Route::get('/register', [AuthenticationController::class, 'viewRegister']);
-
 // Route::post('/create-user', [AuthenticationController::class, 'createUser']);
 
 Route::post('/add-quotation', [QuotationController::class, 'store']);
-
 Route::post('/add-materialquotation', [QuotationController::class, 'addMaterials']);
-
 Route::post('/add-material', [MaterialController::class, 'store'])->name('materials.store');
 
 Route::delete('/quotation-materials/{pivotId}', [QuotationController::class, 'destroy'])->name('quotation-materials.destroy');
+// Drafts
+Route::get('/quotations/drafts', [QuotationController::class, 'drafts'])->name('quotations.drafts');
+
+// Approved
+Route::get('/quotations/approved', [QuotationController::class, 'approved'])->name('quotations.approved');
+
+// Rejected
+Route::get('/quotations/rejected', [QuotationController::class, 'rejected'])->name('quotations.rejected');
 
 Route::post('/quotation-materials/update-quantity', [QuotationController::class, 'updateQuantity'])->name('quotation-materials.updateQuantity');
-
 Route::put('/quotations/{id}/status', [QuotationController::class, 'updateStatus'])->name('quotations.updateStatus');
-
 Route::post('/edit-material', [MaterialController::class, 'update'])->name('materials.update');
 
 Route::get('/materials', [MaterialController::class, 'index'])->name('materials.index');
-
 Route::get('/materials/list', [MaterialController::class, 'list'])->name('materials.list');
-
 Route::post('/materials/update/{id}', [MaterialController::class, 'update'])->name('materials.update');
-
 Route::delete('/materials/{material}', [MaterialController::class, 'destroy'])->name('materials.destroy');
 
 Route::post('/quotations/{id}/update-fee', [QuotationController::class, 'updateFee']);
